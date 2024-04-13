3c73addefc1090284742f00306f72f52d92bf5d7
Mikhail Nepomniashchii <michey85@gmail.com>
2024-04-13 16:50:24 +0200

    cleanup before recording

--- app/blog/actions.ts
+++ app/blog/actions.ts
@@ -1,46 +1,21 @@
 "use server";
-import { prisma } from "@/lib/prisma";
-import type { Post } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function createPost(data: FormData) {
-  const { title, body } = Object.fromEntries(data) as Omit<Post, "id">;
+  const { title, body } = Object.fromEntries(data);
 
-  const post = await prisma.post.create({
-    data: {
-      title,
-      body,
-    },
-  });
-
-  redirect(`/blog/${post.id}`);
+  redirect(`/blog/...`);
 }
 
 export async function updatePost(data: FormData) {
-  const { title, body, id } = Object.fromEntries(data) as Post;
+  const { title, body, id } = Object.fromEntries(data);
 
-  const post = await prisma.post.update({
-    where: {
-      id,
-    },
-    data: {
-      title,
-      body,
-    },
-  });
-
-  revalidatePath(`/blog/${post.id}`);
-  redirect(`/blog/${post.id}`);
+  revalidatePath(`/blog/...`);
+  redirect(`/blog/...`);
 }
 
 export async function removePost(id: string) {
-  await prisma.post.delete({
-    where: {
-      id,
-    },
-  });
-
   revalidatePath("/blog");
   redirect("/blog");
 }
--- services/posts.ts
+++ services/posts.ts
@@ -1,13 +1,3 @@
-import { prisma } from "@/lib/prisma";
+export function getPostById(id: string) {}
 
-export function getPostById(id: string) {
-  return prisma.post.findUnique({
-    where: {
-      id,
-    },
-  });
-}
-
-export function getAllPosts() {
-  return prisma.post.findMany();
-}
+export function getAllPosts() {}
